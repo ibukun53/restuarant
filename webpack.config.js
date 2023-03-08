aa9332e6ed816529255fd44433511bba9664d298
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-03-08 10:27:25 +0100

    Implementing webpack file

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+   
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+},
+ entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  }, 
+  plugins: [new HtmlWebpackPlugin()]
+}
