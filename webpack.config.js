ffa0b6027f03f431b4853c0addc47550bf36cde7
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-03-12 15:58:45 +0100

    Fix eslint error

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-   
   devServer: {
     static: {
-        directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, 'public'),
     },
     compress: true,
     port: 9000,
-},
- entry: './src/index.js',
+  },
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, '/dist'),
     filename: 'index_bundle.js',
-  }, 
-  plugins: [new HtmlWebpackPlugin()]
-}
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
